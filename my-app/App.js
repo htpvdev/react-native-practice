02f0b0a232eafd20218aeca26699d447cb5919ed
htpvdev <haruki20211121@gmail.com>
2023-08-26 23:51:13 +0900

    勤怠管理風アプリ　Expoインストールとスプラッシュ画像導入まで

---
+++ my-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>勤怠アプリ、作るで〜〜〜！</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
